ea2a6eef372057ec9e2ac030894ed00bc8da0230
Guillermo <guillermo.198@gmail.com>
2020-02-19 00:19:56 +0100

    update dto

--- Dtos/UserRegister.cs
+++ Dtos/UserRegister.cs
@@ -3,11 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace pizzeria.Dtos{
     public class UserRegister{
-        [Required]
+        [Required][MaxLength(16)]
         public string Name {get;set;}
-        [Required]
+        [Required][EmailAddress]
         public string Email{get;set;}
-        [Required]
+        [Required][MinLength(8)][MaxLength(16)]
         public string PassWord{get;set;}
     }
 }
