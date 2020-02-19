fcff23420dc9acd7337afe8ebda34a3a253643ba
YannayOrtega <yannay12_86@hotmail.com>
2020-02-19 18:03:36 +0100

    Update IUserRepository.cs

--- Infraestructure/IUserRepository.cs
+++ Infraestructure/IUserRepository.cs
@@ -1,5 +1,9 @@
-﻿namespace pizzeria.Infraestructure
+﻿using pizzeria.Dtos;
+
+namespace pizzeria.Infraestructure
 {
     public interface IUserRepository{
+        void Insert (UserRegister userRegister);
+        void Save (PizzeriaContext pizzeriaContext);
     }
 }
