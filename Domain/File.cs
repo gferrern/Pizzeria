c8cc9b6025ce7657862f1449c4a4698861b17c93
gferrern <gferrern>
2020-02-24 13:49:04 +0100

    correct Upload Service

--- Domain/File.cs
+++ Domain/File.cs
@@ -6,12 +6,13 @@ namespace pizzeria.Domain{
     public class File{
         public Guid Id{get;set;}
         public string FileName {get;set;}
-        public byte[] Filedata{get;set;}
+        public byte[] FileData{get;set;}
 
         public static File Upload(fileUpload fileupload){
             var file = new File();
             file.Id = Guid.NewGuid();
-            file.Filedata = fileupload.filedata;
+            file.FileName = fileupload.FileName;
+            file.FileData = fileupload.FileData;
             return file;
         }
 
