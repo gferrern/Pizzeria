b927b6bca2a54faacafa332ee786fdc68f261616
Guillermo <guillermo.198@gmail.com>
2020-02-24 18:14:37 +0100

    correct and add new files

---
+++ Tests/post/upload.bat
@@ -0,0 +1,3 @@
+curl -H "Content-Type: application/x-www-form-urlencoded" -X POST https://localhost:5001/upload --form "FileData=@../Jellyfish.jpg" --form "FileName=pepe" --output output.user
+
+pause
