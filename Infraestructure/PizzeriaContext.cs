015e3617f5a85e9e640d5fb18c85846cd34b278b
gferrern <gferrern>
2020-02-26 10:43:43 +0100

    correct files

--- Infraestructure/PizzeriaContext.cs
+++ Infraestructure/PizzeriaContext.cs
@@ -2,7 +2,7 @@ using pizzeria.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace pizzeria.Infraestructure{
-    public class PizzeriaContext: DbContext,IUserRepository,IUploadRepository
+    public class PizzeriaContext: DbContext,IUserRepository,IUploadRepository,IIngredientRepository
     {
 
         public PizzeriaContext (DbContextOptions<PizzeriaContext> options)
@@ -21,6 +21,7 @@ namespace pizzeria.Infraestructure{
 
         public DbSet<User> User{get; set;}
         public DbSet<FileUploader> FileUploader{get; set;}
+        public DbSet<Ingredient> Ingredient{get; set;}
 
     }
 }
--- Tests/post/upload.bat
+++ Tests/post/upload.bat
@@ -1,3 +1,3 @@
-curl -X POST https://localhost:5001/upload -H 'cache-control: no-cache' -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' -H 'postman-token: 044f724d-f11d-db49-509d-c705f84ef88a' -F FileName=pepe -F FileData=@../Jellyfish.jpg --output output.user
+curl -X POST https://localhost:5001/upload -H 'cache-control: no-cache' -H 'content-type: multipart/form-data; -F FileName=pepe -F FileData=@../Jellyfish.jpg --output output.user
 
 pause
