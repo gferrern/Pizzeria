4a2b1a7e37405c1939080ad27adac06e604528de
Guillermo <guillermo.198@gmail.com>
2020-03-02 01:12:53 +0100

    reddit

--- src/pizzeria/Controllers/uploadController.cs
+++ src/pizzeria/Controllers/uploadController.cs
@@ -4,7 +4,9 @@ using Microsoft.Extensions.Logging;
 using pizzeria.Dtos;
 using pizzeria.Application;
 using pizzeria.Domain;
-
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+    
 namespace pizzeria.Controllers
 {
     [ApiController]
@@ -15,26 +17,31 @@ namespace pizzeria.Controllers
         private readonly ILogger<UploadController> _logger;
         private readonly IUploadService _UploadService;
 
-        public UploadController(ILogger<UploadController> logger, IUploadService UploadService)
+        private readonly IDistributedCache _cache;
+
+        public UploadController(ILogger<UploadController> logger, IUploadService UploadService, IDistributedCache cache)
         {
             _logger = logger;
             _UploadService = UploadService;
-
+            _cache = cache;
         }
 
         [HttpPost]
-        public IActionResult Upload([FromBody]fileUpload file)
+        public IActionResult upload([FromBody]fileUpload file)
         {
+            Console.WriteLine("pasapollo {0}", file);
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
 
             }
-            var fileUploader = new FileUploader();
-            fileUploader.Id = Guid.NewGuid();
-            _UploadService.Upload(fileUploader);
+            var currentTimeUTC = DateTime.UtcNow.ToString();
+            byte[] encodedCurrentTimeUTC = Encoding.UTF8.GetBytes(currentTimeUTC);
+            var options = new DistributedCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(20));
+            _cache.Set("cachedTimeUTC", encodedCurrentTimeUTC, options);
+            _cache.Set("image", file.FileData, options);
             return Ok();
-
         }
     }
 }
